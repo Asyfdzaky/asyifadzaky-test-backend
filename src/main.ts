c795ef18f2f8ef3ef7e6998d7f0066a5185e6c7e
Asyfdzaky <asyifadzakymaulanaaditria@mail.ugm.ac.id>
2025-12-18 15:06:12 +0700

    build: add NestJS application bootstrap and remove Vercel deployment configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import serverless from 'serverless-http';
-import express from 'express';
-
-let server: any;
 
 async function bootstrap() {
-  if (!server) {
-    const expressApp = express();
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-    );
-
-    app.enableCors();
-    await app.init();
-
-    server = serverless(expressApp);
-  }
-
-  return server;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(process.env.DATABASE_URL);
 }
-
-export default async function handler(req: any, res: any) {
-  const server = await bootstrap();
-  return server(req, res);
-}
+bootstrap();
